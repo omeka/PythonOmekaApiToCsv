540db9047cee5ac6f925dc2d145b4d4eb0d51d09
John Flatness <john@zerocrates.org>
2020-02-17 15:48:52 -0500

    Support Python 3
    Strips out the mostly unneeded Omeka-specific client code and usage of
    httplib2.
    
    Also shifts to some saner defaults like empty cells instead of "None".

--- omekacsv.py
+++ omekacsv.py
@@ -1,12 +1,21 @@
 #! /usr/bin/python
 
-from omekaclient import OmekaClient
-from httplib2 import ServerNotFoundError
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
 import csv
 import json
 import math
 import time
 import readline
+
+try:
+    from urllib.parse import urlencode
+    from urllib.request import urlopen
+    from urllib.error import URLError, HTTPError
+except ImportError:
+    from urllib import urlencode
+    from urllib2 import urlopen, URLError, HTTPError
 
 '''
 Extract top-level metadata and element_texts from items returned by
@@ -16,40 +25,48 @@ requests to items, collections, element sets, elements, files, tags, exhibits, a
 Based on Caleb McDaniel's original Python CSV file generator: https://github.com/wcaleb/omekadd
 '''
 
+try:
+    input = raw_input
+except NameError:
+    pass
+
+try:
+    unicode
+    py2 = True
+except NameError:
+    unicode = str
+    py2 = False
+
 endpoint = ''
 while not endpoint:
-    endpoint = raw_input('Enter your Omeka API endpoint\n')
-apikey = raw_input('If you are using an API key, please enter it now. Otherwise press Enter.\n')
+    endpoint = input('Enter your Omeka API endpoint\n')
+apikey = input('If you are using an API key, please enter it now. Otherwise press Enter.\n')
 if not apikey:
     apikey = None
 
 available_resources = ['items', 'files', 'elements', 'element_sets', 'tags', 'exhibits', 'exhibit_pages']
 for resource in available_resources:
-    print 'Exporting ' + resource
-
+    print('Exporting ' + resource)
     def request(query={}):
-        try:
-            response, content = OmekaClient(endpoint, apikey).get(resource, None, query)
-            if response.status != 200:
-                print response.status, response.reason
-                exit()
-            else:
-                return response, content
-        except ServerNotFoundError:
-            print 'The server was not found. Please check your endpoint and try again.'
-            exit()
+        url = endpoint + "/" + resource
+        if apikey is not None:
+            query["key"] = apikey
+        url += "?" + urlencode(query)
 
+        response = urlopen(url)
+        return response.info(), response.read()
     def unicodify(v):
+        if type(v) is list or type(v) is dict:
+            return None
         if type(v) is bool or type(v) is int:
            return unicode(v)
-        else:
-           return v
+        return v
 
     def get_all_pages(pages):
         global data
         page = 1
         while page <= pages:
-            print 'Getting results page ' + str(page) + ' of ' + str(pages) + ' ...'
+            print('Getting results page ' + str(page) + ' of ' + str(pages) + ' ...')
             response, content = request({'page': str(page), 'per_page': '50'})
             data.extend(json.loads(content))
             page += 1
@@ -58,43 +75,55 @@ for resource in available_resources:
     # make initial API request; get max pages
     response, content = request()
     pages = int(math.ceil(float(response['omeka-total-results'])/50))
+    pages = 1
 
     # declare global variables; get all pages
     fields = []
     data = []
     get_all_pages(pages)
+    csv_rows = []
 
     for D in data:
-        if 'tags' in D and D['tags']:
-            tags = [ d['name'] for d in D['tags'] ]
-            D['tags'] = ', '.join(tags)
-        if 'element_texts' in D:
-            for d in D['element_texts']:
-                k = d['element']['name']
-                v = d['text']
-                D[k] = v
-        if 'page_blocks' in D:
-                text = [ d['text'] for d in D['page_blocks'] ]
-                D['Text'] = ' | '.join(filter(None, text))
+        csv_row = {}
+
         for k, v in D.items():
-            D[k] = unicodify(v)
-            if D[k] and type(v) is dict:
-                for key, value in v.items():
-                    D[k + '_' + key] = unicodify(D[k][key])
-            if type(v) is list or type(v) is dict:
-                del D[k] 
-            if v == None:
-                del D[k]
-        for k in D.keys():
+            if k == 'tags':
+                tags = [ tag['name'] for tag in v ]
+                csv_row['tags'] = ','.join(tags)
+            elif k == 'element_texts':
+                for element_text in v:
+                    csv_row[element_text['element']['name']] = element_text['text']
+            elif k == 'page_blocks':
+                text = [ block['text'] for block in v ]
+                csv_row['Text'] = ' | '.join(filter(None, text))
+            elif k == 'owner':
+                csv_row['owner_id'] = unicodify(v['id'])
+            elif type(v) is dict:
+                for subkey, subvalue in v.items():
+                    subvalue_string = unicodify(subvalue)
+                    if subvalue_string is not None:
+                        csv_row[k + '_' + subkey] = subvalue_string
+                continue;
+            elif type(v) is list or v is None:
+                continue;
+            else:
+                csv_row[k] = unicodify(v)
+
+        for k in csv_row.keys():
             if k not in fields: fields.append(k)
+        csv_rows.append(csv_row)
 
-    # write to CSV output file using DictWriter instance
-    # by default, fill empty cells with 'None'; un-quote None for empty cell
-    o = open(resource + '_output.csv', 'w')
-    c = csv.DictWriter(o, [f.encode('utf-8', 'replace') for f in sorted(fields)], restval='None', extrasaction='ignore') 
-    c.writeheader()
-    for D in data:
-        c.writerow({k:v.encode('utf-8', 'replace') for k,v in D.items() if isinstance(v, unicode)})
-
+    if (py2):
+        o = open(resource + '_output.csv', 'w')
+        c = csv.DictWriter(o, [f.encode('utf-8', 'replace') for f in sorted(fields)], extrasaction='ignore') 
+        c.writeheader()
+        for row in csv_rows:
+            c.writerow({k:v.encode('utf-8', 'replace') for k,v in row.items() if isinstance(v, unicode)})
+    else:
+        o = open(resource + '_output.csv', 'w', encoding = 'utf-8')
+        c = csv.DictWriter(o, sorted(fields), extrasaction='ignore')
+        c.writeheader()
+        for row in csv_rows:
+            c.writerow(row)
     o.close()
-    print 'File created: ' + resource + '_output.csv'
+    print('File created: ' + resource + '_output.csv')
